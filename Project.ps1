9503a9d7fa8a605c204ae90ded7a68d6b8ea607b
wilbrinkdesign <mark@wilbrink.design>
2022-01-27 12:54:51 +0100

    Update Project.ps1

--- Project.ps1
+++ Project.ps1
@@ -8,7 +8,7 @@
     .NOTES
     Author:   Mark Wilbrink
     Created:  16-1-2022
-    Modified: 24-1-2022
+    Modified: 27-1-2022
 #>
 
 # Globale vars die je in de verschillende functies kunt gebruiken
