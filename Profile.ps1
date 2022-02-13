130b98d2189ac29937812eb812b27c2e9030e3b0
Mark Wilbrink <98471282+wilbrinkdesign@users.noreply.github.com>
2022-02-13 15:26:54 +0100

    Update Profile.ps1

--- Profile.ps1
+++ Profile.ps1
@@ -1,2 +1,2 @@
-# Het projecten script inladen zodra je PowerShell start => C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1
+# Het projecten script inladen zodat alle functies beschikbaar worden zodra je PowerShell start => C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1
 . "D:\OneDrive\Wilbrink Design\Scripts\Project.ps1"
