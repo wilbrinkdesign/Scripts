2b26c0aac7b0b6261d6e409165f16e302939f944
Mark Wilbrink <mark@wilbrink.design>
2022-07-06 22:45:44 +0200

    Create Unieke bezoekers.ps1

---
+++ Windows Server/IIS/Unieke bezoekers.ps1
@@ -0,0 +1,33 @@
+<#
+    .Synopsis
+        Converts plain text IIS logs into a ps Object
+    .DESCRIPTION
+        Converts plain text IIS logs into a ps Object
+    .EXAMPLE
+        Get-ChildItem '<path to logs>\*.log' | Convert-IISLogsToObject | Sort-Object c-ip -Unique
+    .EXAMPLE
+        Convert-IISLogsToObject -path (Get-ChildItem '<path to logs>\*log') | Where-Object { $_.'cs-username' -eq '<userName>' } | Sort-Object c-ip -Unique
+    .NOTES
+        General notes
+    .AUTHOR
+        Ben Taylor - 09/07/2016
+#>
+function Convert-IISLogsToObject {
+    [CmdletBinding()]
+    [OutputType([System.Management.Automation.PSCustomObject])]
+    Param(
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [ValidateScript({ Test-Path -Path $_ })]
+        [string[]]
+        $path
+    )
+
+    Process {
+        forEach($filePath in $path) {
+            $headers = (Get-Content -Path $filePath -TotalCount 4 | Select -First 1 -Skip 3) -replace '#Fields: ' -split ' '
+            Get-Content $filePath | Select-String -Pattern '^#' -NotMatch | ConvertFrom-Csv -Delimiter ' ' -Header $headers
+        }
+    }
+}
+
+Get-ChildItem 'C:\inetpub\logs\LogFiles\W3SVC1\*.log' | Convert-IISLogsToObject | Sort-Object c-ip -Unique | Select-Object date, time, s-ip, c-ip, cs-uri-stem, cs-username | Format-Table
