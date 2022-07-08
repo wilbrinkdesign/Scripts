79d655fdbe925ee4d9918dd61e6d87accd29d271
Mark Wilbrink <98471282+wilbrinkdesign@users.noreply.github.com>
2022-07-08 11:36:49 +0200

    Update Azure user aanmaken.sql

--- Databases/SQL/Azure user aanmaken.sql
+++ Databases/SQL/Azure user aanmaken.sql
@@ -1,11 +1,6 @@
--- create SQL login in master database
-CREATE LOGIN azureituser
-WITH PASSWORD = 'password';
-
--- add database user for login testLogin1 => change to the correct database
-CREATE USER azureituser
-FROM LOGIN azureituser
-WITH DEFAULT_SCHEMA=dbo;
-
--- add user to database role(s) (i.e. db_owner)
-ALTER ROLE db_owner ADD MEMBER azureituser;
+-- create SQL auth login from master  
+CREATE LOGIN user1 WITH PASSWORD = 'password' 
+-- select your db in the dropdown and create a user mapped to a login  
+CREATE USER user1 FOR LOGIN ictwodvwesql WITH DEFAULT_SCHEMA = dbo;  
+-- add user to role(s) in db  
+ALTER ROLE db_owner ADD MEMBER user1;   
